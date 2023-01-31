fe07ba2af322deb0b0c155d6363e8a12e814794d
Jack Fung <fungsaulok524@gmail.com>
2023-01-31 21:34:04 +0800

    change to hide percentage by default

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		var cmds []tea.Cmd
 		var cmd tea.Cmd
 
-		if m.showpercentage {
+		if !m.showpercentage {
 			m.progress.ShowPercentage = false
 		}
 
